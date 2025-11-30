462a60c7b48458b5f6ef2dd07059ae5a20c9d6ec
Marek Drábek <marek.drabek@easyidm.cz>
2025-11-30 22:39:08 +0100

    Parser

---
+++ parser.c
@@ -0,0 +1,5 @@
+//
+// Created by Marek on 30.11.2025.
+//
+
+#include "parser.h"
---
+++ parser.h
@@ -0,0 +1,8 @@
+//
+// Created by Marek on 30.11.2025.
+//
+
+#ifndef IFJ_COMPILER_PARSER_H
+#define IFJ_COMPILER_PARSER_H
+
+#endif //IFJ_COMPILER_PARSER_H
